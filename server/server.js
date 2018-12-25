f6701b5816c525e791bcd0d893e9336bc55d7940
MELVERTON\Mel <huntermelverton@gmail.com>
2018-12-24 19:13:50 -0500

    mongoconnect

--- server/server.js
+++ server/server.js
@@ -7,14 +7,11 @@ const passport = require('passport');
 const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
-
-// Create a new Express application
+const key = require('../config/key.js');
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://huntermelverton:hunter11@ds157843.mlab.com:57843/melvertondb';
+const MONGO_URI = key.key;
 
-// Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
 // Connect to the mongoDB instance and log a message
